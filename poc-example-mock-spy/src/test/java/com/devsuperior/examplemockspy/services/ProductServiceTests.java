4359275cbd0014236b8088435a7159940c874c37
auriceliof <auricelio.suporte@hotmail.com>
2023-11-03 20:16:17 -0300

    05-07 Simulando comportamento repository com Mockito

--- poc-example-mock-spy/src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
+++ poc-example-mock-spy/src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -1,11 +1,20 @@
 package com.devsuperior.examplemockspy.services;
 
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.examplemockspy.dto.ProductDTO;
+import com.devsuperior.examplemockspy.entities.Product;
 import com.devsuperior.examplemockspy.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -15,5 +24,35 @@ public class ProductServiceTests {
 	
 	@Mock
 	private ProductRepository repository;
-
+	
+	private Long existingId, nonExistingId;
+	private Product product;
+	private ProductDTO productDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		
+		existingId = 1L;
+		nonExistingId = 2L;
+		
+		product = new Product(1L, "Playstation", 2000.0);
+		productDTO = new ProductDTO(product);
+		
+		Mockito.when(repository.save(any())).thenReturn(product);
+		
+		Mockito.when(repository.getReferenceById(existingId)).thenReturn(product);
+		Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
